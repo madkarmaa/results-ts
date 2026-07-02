80cb1068603560151791fca6a41ab9320a4b246e
madkarmaa <madkarma99@gmail.com>
2026-07-02 17:21:04 +0200

    feat: add `tryBlock` and `tryBlockAsync` helpers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './result';
 export * from './option';
+export * from './try-block';
 export { type AsyncResult, catchUnwindAsync } from './async-result';
 export { type AsyncOption } from './async-option';
---
+++ src/try-block.ts
@@ -0,0 +1,73 @@
+import { Err, Ok, type Result } from './result';
+import { isResult } from './utils';
+
+type ErrOf<R> = R extends Result<unknown, infer E> ? E : never;
+
+type Question = <T, E>(
+    result: Result<T, E>
+) => Generator<Result<never, E>, T, unknown>;
+
+type AsyncQuestion = {
+    <T, E>(result: Result<T, E>): Generator<Result<never, E>, T, unknown>;
+    <T, E>(
+        result: PromiseLike<Result<T, E>>
+    ): AsyncGenerator<Result<never, E>, T, unknown>;
+};
+
+function $<T, E>(result: Result<T, E>): Generator<Result<never, E>, T, unknown>;
+function $<T, E>(
+    result: PromiseLike<Result<T, E>>
+): AsyncGenerator<Result<never, E>, T, unknown>;
+function $<T, E>(
+    result: Result<T, E> | PromiseLike<Result<T, E>>
+):
+    | Generator<Result<never, E>, T, unknown>
+    | AsyncGenerator<Result<never, E>, T, unknown> {
+    if (isResult<T, E>(result))
+        return (function* (): Generator<Result<never, E>, T, unknown> {
+            if (result.isOk()) return result.unwrap();
+            yield Err(result.unwrapErr());
+            throw new Error('unreachable');
+        })();
+
+    return (async function* (): AsyncGenerator<Result<never, E>, T, unknown> {
+        const awaited = await result;
+        if (awaited.isOk()) return awaited.unwrap();
+        yield Err(awaited.unwrapErr());
+        throw new Error('unreachable');
+    })();
+}
+
+export function tryBlock<T, Y extends Result<unknown, unknown>>(
+    fn: ($: Question) => Generator<Y, T, unknown>
+): Result<T, ErrOf<Y>> {
+    const iterator = fn($);
+    let state = iterator.next();
+
+    while (!state.done) {
+        const result = state.value;
+        if (!isResult(result))
+            throw new TypeError('tryBlock yielded a non-Result value');
+        if (result.isErr()) return Err(result.unwrapErr() as ErrOf<Y>);
+        state = iterator.next(result.unwrap());
+    }
+
+    return Ok(state.value);
+}
+
+export async function tryBlockAsync<T, Y extends Result<unknown, unknown>>(
+    fn: ($: AsyncQuestion) => AsyncGenerator<Y, T, unknown>
+): Promise<Result<T, ErrOf<Y>>> {
+    const iterator = fn($);
+    let state = await iterator.next();
+
+    while (!state.done) {
+        const result = state.value;
+        if (!isResult(result))
+            throw new TypeError('tryBlockAsync yielded a non-Result value');
+        if (result.isErr()) return Err(result.unwrapErr() as ErrOf<Y>);
+        state = await iterator.next(result.unwrap());
+    }
+
+    return Ok(state.value);
+}
