9ff37425b18e13ed30a71903b601c61984e3e588
madkarmaa <madkarma99@gmail.com>
2026-07-02 23:01:17 +0200

    refactor: name tryBlock generators

--- src/try-block.ts
+++ src/try-block.ts
@@ -14,6 +14,23 @@ type AsyncQuestion = {
     ): AsyncGenerator<Result<never, E>, T, unknown>;
 };
 
+function* resultQuestion<T, E>(
+    result: Result<T, E>
+): Generator<Result<never, E>, T, unknown> {
+    if (result.isOk()) return result.unwrap();
+    yield Err(result.unwrapErr());
+    throw new Error('unreachable');
+}
+
+async function* asyncResultQuestion<T, E>(
+    result: PromiseLike<Result<T, E>>
+): AsyncGenerator<Result<never, E>, T, unknown> {
+    const awaited = await result;
+    if (awaited.isOk()) return awaited.unwrap();
+    yield Err(awaited.unwrapErr());
+    throw new Error('unreachable');
+}
+
 function $<T, E>(result: Result<T, E>): Generator<Result<never, E>, T, unknown>;
 function $<T, E>(
     result: PromiseLike<Result<T, E>>
@@ -23,19 +40,8 @@ function $<T, E>(
 ):
     | Generator<Result<never, E>, T, unknown>
     | AsyncGenerator<Result<never, E>, T, unknown> {
-    if (isResult<T, E>(result))
-        return (function* (): Generator<Result<never, E>, T, unknown> {
-            if (result.isOk()) return result.unwrap();
-            yield Err(result.unwrapErr());
-            throw new Error('unreachable');
-        })();
-
-    return (async function* (): AsyncGenerator<Result<never, E>, T, unknown> {
-        const awaited = await result;
-        if (awaited.isOk()) return awaited.unwrap();
-        yield Err(awaited.unwrapErr());
-        throw new Error('unreachable');
-    })();
+    if (isResult<T, E>(result)) return resultQuestion(result);
+    return asyncResultQuestion(result);
 }
 
 export function tryBlock<T, Y extends Result<unknown, unknown>>(
