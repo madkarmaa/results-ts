031e8deb4411775c34dbeca77a0261ac498cfe4f
madkarmaa <madkarma99@gmail.com>
2026-07-11 14:52:40 +0200

    perf: remove PanicError's stack trace

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,12 @@
 export class PanicError extends Error {
     constructor(message: string, options?: ErrorOptions) {
+        const hasStackTraceLimit = typeof Error.stackTraceLimit === 'number';
+        const previousLimit = Error.stackTraceLimit;
+
+        if (hasStackTraceLimit) Error.stackTraceLimit = 0;
         super(message, options);
+        if (hasStackTraceLimit) Error.stackTraceLimit = previousLimit;
+
         this.name = 'PanicError';
     }
 }
